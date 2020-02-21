c9b2c5d22781b3258feba0416859a3fd90214aaa
Bruce <gkm85663@gmail.com>
2020-02-22 01:49:53 +0800

    Pass test  Ctor_WhenViewIsLoaded_CallsViewRender,  p.126~127

---
+++ Presenter.UnitTest/PresenterTests.cs
@@ -0,0 +1,44 @@
+using System;
+using NSubstitute;
+using NSubstitute.Core.Events;
+using NUnit.Framework;
+
+namespace PresenterTests.UnitTest
+{
+    public class PresenterTests
+    {
+        // p.126~127
+        [Test]
+        public void Ctor_WhenViewIsLoaded_CallsViewRender()
+        {
+            var mockView = Substitute.For<IView>();
+            var presenter = new Presenter(mockView);
+
+            mockView.Loaded += Raise.Event<Action>();
+            mockView.Received().Render(Arg.Is<string>(x => x.Contains("Hello world")));
+        }
+    }
+
+    public class Presenter
+    {
+        private readonly IView _view;
+
+        public Presenter(IView view)
+        {
+            _view = view;
+            _view.Loaded += OnLoaded;
+        }
+
+        public void OnLoaded()
+        {
+            _view.Render("Hello world11");
+        }
+    }
+
+    public interface IView
+    {
+        event Action Loaded;
+
+        void Render(string text);
+    }
+}
